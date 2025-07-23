aad42522d03b227e888b704dd379d61a00ddd886
VMahurkar94044 <vaishnavimahurkar23@gmail.com>
2025-07-23 01:57:59 -0700

    youtube all stateless done

---
+++ lib/controller/tabs_controller.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_3/screens/library.dart';
+import 'package:flutter_application_3/screens/shorts.dart';
+import 'package:flutter_application_3/screens/subscriptions.dart';
+import 'package:flutter_application_3/screens/youtube_HP.dart';
+import 'package:get/get.dart';
+
+class Tabcontroller extends GetxController
+{
+  var selectedIndex  = 0.obs;
+
+   final RxList<Widget> Screens =[
+    HomePage(),
+    Shorts(),
+    Center(child: Icon(Icons.add_circle_outline, size: 80, color: Colors.black)),
+    Subscriptions(),
+    //Center(child: Text("Subscriptions", style: TextStyle(color: Colors.black))),
+    Lib(),
+    ].obs;
+
+  void onItemTapped (int index){
+    
+    selectedIndex.value = index;
+  }
+}
--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -1,65 +1,3 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter_application_3/color/app_color.dart';
-// import 'package:flutter_application_3/screens/library.dart';
-// import 'package:flutter_application_3/screens/shorts.dart';
-// import 'package:flutter_application_3/screens/subscriptions.dart';
-// import 'package:flutter_application_3/screens/youtube_HP.dart';
-
-// class Tabs extends StatelessWidget {
-//   const Tabs({super.key});
-
-//   int _selectedIndex  = 0;
-//    final List<Widget> _Screens =[
-//     HomePage(),
-//     Shorts(),
-//     Center(child: Icon(Icons.add_circle_outline, size: 80, color: Colors.black)),
-//     Subscriptions(),
-//     //Center(child: Text("Subscriptions", style: TextStyle(color: Colors.black))),
-//     Lib(),
-//     ];
-
-//   void _onItemTapped (int index){
-//     setState(() {
-//         _selectedIndex = index;
-//     });
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-
-//       body:   _Screens[_selectedIndex],
-//       bottomNavigationBar: BottomNavigationBar(
-//         backgroundColor: Colors.black,
-//         selectedIconTheme: IconThemeData(color: Appcolor.textcolor),
-//         selectedItemColor: Appcolor.textcolor,
-//         currentIndex: _selectedIndex,
-//         onTap: _onItemTapped,
-//         type: BottomNavigationBarType.fixed,
-//         unselectedItemColor: Appcolor.subtextcolor,
-//         items: const [
-//           BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-//           BottomNavigationBarItem(
-//             icon: Icon(Icons.play_arrow_rounded),
-//             label: "Shorts",
-//           ),
-//           BottomNavigationBarItem(
-//             icon: Icon(Icons.add_circle_outline,size:30),
-//             label: '',
-//           ),
-//           BottomNavigationBarItem(
-//             icon: Icon(Icons.subscriptions),
-//             label: 'Subscriptions',
-//           ),
-//           BottomNavigationBarItem(
-//             icon: Icon(Icons.video_library),
-//             label: 'Library',
-//           ),
-          
-//         ],
-//       ),
-//     );
-//   }
-// }
 
 
 
@@ -67,48 +5,34 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_application_3/color/app_color.dart';
-import 'package:flutter_application_3/screens/library.dart';
-import 'package:flutter_application_3/screens/shorts.dart';
-import 'package:flutter_application_3/screens/subscriptions.dart';
-import 'package:flutter_application_3/screens/youtube_HP.dart';
+import 'package:flutter_application_3/controller/tabs_controller.dart';
+//import 'package:flutter_application_3/screens/library.dart';
+//import 'package:flutter_application_3/screens/shorts.dart';
+//import 'package:flutter_application_3/screens/subscriptions.dart';
+//import 'package:flutter_application_3/screens/youtube_HP.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
 
 
-class Tabs extends StatefulWidget {
-  const Tabs({super.key});
+ class Tabs extends StatelessWidget {
 
-  @override
-  State<Tabs> createState() => _TabsState();
-}
+  final tc = Get.put(Tabcontroller());
+   Tabs({super.key});
 
-class _TabsState extends State<Tabs> {
-
-  int _selectedIndex  = 0;
-   final List<Widget> _Screens =[
-    HomePage(),
-    Shorts(),
-    Center(child: Icon(Icons.add_circle_outline, size: 80, color: Colors.black)),
-    Subscriptions(),
-    //Center(child: Text("Subscriptions", style: TextStyle(color: Colors.black))),
-    Lib(),
-    ];
-
-  void _onItemTapped (int index){
-    setState(() {
-        _selectedIndex = index;
-    });
-  
-  }
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return  
+    Obx((){
+      return
+    Scaffold(
 
-      body:   _Screens[_selectedIndex],
+      body:   tc.Screens[tc.selectedIndex.value],
       bottomNavigationBar: BottomNavigationBar(
         backgroundColor: Colors.black,
         selectedIconTheme: IconThemeData(color: Appcolor.textcolor),
         selectedItemColor: Appcolor.textcolor,
-        currentIndex: _selectedIndex,
-        onTap: _onItemTapped,
+        currentIndex: tc.selectedIndex.value,//_selectedIndex,
+        onTap: tc.onItemTapped,
         type: BottomNavigationBarType.fixed,
         unselectedItemColor: Appcolor.subtextcolor,
         items: const [
@@ -132,6 +56,8 @@ class _TabsState extends State<Tabs> {
           
         ],
       ),
+    );
+    }
     );
   }
 }
